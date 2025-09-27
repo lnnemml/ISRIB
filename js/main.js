ef980b9416b0fa3b1f124670d769da2c446feaa1
lnnemml <lnnemml@gmail.com>
2025-09-27 18:37:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function initializeApp() {
+  bindBurgerMenu?.();
   initHeaderBehavior();
   initSmoothScrolling();
   initFadeInAnimations();
