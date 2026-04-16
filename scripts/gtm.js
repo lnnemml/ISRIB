273319dec5848e4748d92b772f2b24b844e277f9
lnnemml <lnnemml@gmail.com>
2026-04-16 12:20:58 +0300

    вке

--- scripts/gtm.js
+++ scripts/gtm.js
@@ -3,3 +3,17 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M2QCB45Q');
+
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,"clarity","script","wci5xmxfnu");
+
+(function(){
+  var utm = window.ISRIBTracking && window.ISRIBTracking.getUTM
+    ? window.ISRIBTracking.getUTM() : {};
+  if (utm.utm_content)  window.clarity("set","creative",utm.utm_content);
+  if (utm.utm_source)   window.clarity("set","source",utm.utm_source);
+  if (utm.utm_campaign) window.clarity("set","campaign",utm.utm_campaign);
+})();
